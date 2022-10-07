14d259c3adf68f3dec4f582ab1dcaf3cd11a73d2
agustin rosano <agustinrosano7@gmail.com>
2022-10-07 14:36:35 -0300

    se agrego

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,41 +1,63 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore, query} from "firebase/firestore"
-import {collection, getDocs,   where } from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
+import {
+	collection,
+	doc,
+	getDocs,
+	getDoc,
+	query,
+	where,
+} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCjd5DkR_KcYlBuLmLnJcvWKtd_3IPh6_8",
-  authDomain: "e-comm-cerve.firebaseapp.com",
-  projectId: "e-comm-cerve",
-  storageBucket: "e-comm-cerve.appspot.com",
-  messagingSenderId: "961759771047",
-  appId: "1:961759771047:web:7f518d5cceade30d731e82"
+	apiKey: "AIzaSyAFRr-sr9JCEM_9s_UPdd0i2UIxmkDF-SE",
+	authDomain: "react-ecomm-coderhouse.firebaseapp.com",
+	projectId: "react-ecomm-coderhouse",
+	storageBucket: "react-ecomm-coderhouse.appspot.com",
+	messagingSenderId: "412607354277",
+	appId: "1:412607354277:web:80617b4e0f5f0e5d0dc44c",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
 
-export  const firestoreFetch = async (id) =>{ 
-let q ;
+export const firestoreFetch =  async(categoryId) => {
+	let q = query(collection(db, "products"));
+	if (categoryId) {
+		q = query(
+			collection(db, "products"),
+			where("products", "==", categoryId)
+		);
+	} else {
+		q = query(collection(db, "products"));
+	}
+	const querySnapshot = await getDocs(q);
+	const dataFromFirestore = querySnapshot.docs.map((doc) => {
+		return {
+			id: doc.id,
+			...doc.data(),
+		};
+	});
+	return dataFromFirestore;
+};
 
-if (id){
-    q = query (collection (db, "products"), where ('categoryId', ' == ', id));
+export const getOneItem = async (id) => {
+	const docRef = doc(db, "products", id);
+	const docSnap = await getDoc(docRef);
 
-} else  {
+	if (docSnap.exists()) {
+        const item = {
+            id : docSnap.id,
+            ...docSnap.data()
+        }
+        return item;
 
-    q = query (collection (db, "products"))
-}
-
-const querySnapshot = await getDocs(q);
-
-const dataFromFirestore = querySnapshot.docs.map (doc => ({
-    id: doc.id,
-    ...doc.data()
-}))
-return dataFromFirestore
- 
-}
+	} else {
+		// doc.data() will be undefined in this case
+		console.log("No such document!");
+	}
+};
